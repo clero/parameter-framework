e75d5419659b72d5bd3489ce3b4b6f87ff97f7f3
Jules Clero <julesx.clero@intel.com>
2015-05-07 18:45:31 +0200

    tuple use test
    Signed-off-by: Jules Clero <julesx.clero@intel.com>

--- test-serialization/Serializer.cpp
+++ test-serialization/Serializer.cpp
@@ -2,8 +2,15 @@
 #include <map>
 #include <list>
 #include <string>
+#include <tuple>
 #include <functional>
 
+//template <template <class> class Wrapper, class... TypeList>
+//inline auto make_wrapped_tuple(Wrapper<TypeList>&&... wrapped)
+//    -> decltype(std::make_tuple(wrapped...))
+//{
+//    return std::make_tuple(wrapped...);
+//}
 /** Data member setter to use during deserialization
  *
  * @tparam the type of the data to retrieve
@@ -22,37 +29,76 @@ struct Attribute
     const Setter<T> setter;
 };
 
+//template <class... TypeList> using Attributes = std::tuple<Attribute<TypeList>...>;
+
 /** Attributes are list of attribute of differrent Setter type */
-template <class... TypeNull> struct Attributes {};
-template <class Head, class... Tail>
-struct Attributes<Head, Tail...> : Attributes<Tail...>
+
+template <class T>
+inline Attribute<T> make_attribute(const std::string &&name, const Setter<T> &setter)
 {
-    Attributes(Attribute<Head> head, Attribute<Tail>... tail) :
-        Attributes<Tail...>(tail...), front(head)
-    {
-    }
+    return {name, setter};
+}
 
-    Attribute<Head> front;
-};
-
-template <class... TypeList> struct Node;
-template <class... TypeList> using Nodes = std::list<Node<TypeList...> >;
-
-/** Xml tag representation */
 template <class... TypeList>
-struct Node
+inline auto make_attributes(TypeList&&... attribute)
+    -> decltype(std::make_tuple(attribute...))
 {
-    std::string tag;
-    Attributes<TypeList...> attributes;
-    Nodes<TypeList...> childs;
-};
+    return std::make_tuple(attribute...);
+}
+
+
+template <class... TypeList>
+inline auto make_node(const std::string&&, TypeList&&... node)
+    -> decltype(std::make_tuple(attribute...))
+{
+    return std::make_tuple(attribute...);
+}
+//template <class... TypeNull> struct Nodes {};
+//template <class Head, class... Tail>
+//struct Nodes<Head, Tail...> : Nodes<Tail...>
+//{
+//    Nodes(Node<Head> head, Node<Tail>... tail) :
+//        Nodes<Tail...>(tail...), front(head)
+//    {
+//    }
+//
+//    Node<Head> front;
+//};
+
+//template <class... TypeList> struct Node;
+////template <class... TypeList> using Nodes = std::list<Node<TypeList...> >;
+//
+///** Xml tag representation */
+//template <class... TypeNull> struct Node {};
+//template <template <class... SubTypeList> class Head, class... Tail>
+//struct Node<Head<SubTypeList>, Tail...> : Node<Tail...>
+//{
+//    Node(Head<SubTypeList> head, Node<Tail>... childs) :
+//        Node<Tail...>(childs...), childs(head)
+//    {
+//    }
+//
+//    std::string tag;
+//    //Head attributes;
+//    Node<Tail...> childs;
+//};
+
+//template <template <class... TypeList> class AttributesTemplate, class... ChildTypeList> struct Node;
+//template <template <class... TypeList> class AttributesTemplate, class... ChildTypeList>
+//struct Node
+//{
+//
+//    const std::string name;
+//    AttributesTemplate<TypeList> attributes;
+//    std::list<Node<ChildTypeList...> > childs;
+//}
 
 
 template <class... TypeList>
 class Serializer
 {
-    Serializer(const std::string &xmlFile, Node<TypeList...> rootNode)
-    {
+    //Serializer(const std::string &xmlFile, Node<TypeList...> rootNode)
+    //{
         //CXmlSerializingContext context(error);
         //CXmlFileDocSource source(_configurationFile,
         //                         "",//_schemasLocation + "/ParameterFrameworkConfiguration.xsd",
@@ -82,7 +128,7 @@ class Serializer
         // Read root tag
         // call good setters
         // call an other serializer on the next tag
-    }
+    //}
 
     //using XmlNode = std::string; // temporary
 
@@ -101,9 +147,9 @@ public:
     std::string getB() { return mB; };
     bool getC() { return mC; };
 
-    void setA(int a) { mA = a; };
-    void setB(std::string b) { mB = b; };
-    void setC(bool c) { mC = c; };
+    Setter<int> sA = [this](int a){ mA = a; };
+    Setter<std::string> sB = [this](std::string b){ mB = b; };
+    Setter<bool> sC = [this](bool c){ mC = c; };
 
 private:
     int mA = 0;
@@ -114,27 +160,35 @@ private:
 int main()
 {
     FillMe toFill;
-    FillMe pleaseFill;
-    Nodes<int, std::string, bool> nodes{
-        {
-            "FillMe",
-            {
-                {"A", [&toFill](int a){ toFill.setA(a); }},
-                {"B", [&toFill](std::string b){ toFill.setB(b); }},
-                {"C", [&toFill](bool c){ toFill.setC(c); } }
-            },
-            {} //empty child tag list
-        },
-        {
-            "FillIt",
-            {
-                {"A", [&pleaseFill](int a){ pleaseFill.setA(a); }},
-                {"B", [&pleaseFill](std::string b){ pleaseFill.setB(b); }},
-                {"C", [&pleaseFill](bool c){ pleaseFill.setC(c); } }
-            },
-            {} //empty child tag list
-        }
-    };
+    auto attr = make_attribute("A", toFill.sA);
+
+    auto attributes = make_attributes(
+                        make_attribute("A", toFill.sA),
+                        make_attribute("B", toFill.sB),
+                        make_attribute("C", toFill.sC)
+                        );
+
+    //FillMe pleaseFill;
+    //Nodes<int, std::string, bool> nodes{
+    //    {
+    //        "FillMe",
+    //        {
+    //            {"A", [&ill](int a){ toFill.setA(a); }},
+    //            {"B", [&toFill](std::string b){ toFill.setB(b); }},
+    //            {"C", [&toFill](bool c){ toFill.setC(c); } }
+    //        },
+    //        {} //empty child tag list
+    //    },
+    //    {
+    //        "FillIt",
+    //        {
+    //            {"A", [&pleaseFill](int a){ pleaseFill.setA(a); }},
+    //            {"B", [&pleaseFill](std::string b){ pleaseFill.setB(b); }},
+    //            {"C", [&pleaseFill](bool c){ pleaseFill.setC(c); } }
+    //        },
+    //        {} //empty child tag list
+    //    }
+    //};
 
 
     // ok
@@ -144,15 +198,15 @@ int main()
     //    {"B", [&toFill](std::string b){ toFill.setB(b); }},
     //    {"C", [&toFill](bool c){ toFill.setC(c); }}
     //};
-    Node<int, std::string, bool> tag{
-        "FillMe",
-        {
-            {"A", [&toFill](int a){ toFill.setA(a); }},
-            {"B", [&toFill](std::string b){ toFill.setB(b); }},
-            {"C", [&toFill](bool c){ toFill.setC(c); } }
-        },
-        {} //empty child tag list
-    };
+    //Node<int, std::string, bool> tag{
+    //    "FillMe",
+    //    {
+    //        {"A", [&toFill](int a){ toFill.setA(a); }},
+    //        {"B", [&toFill](std::string b){ toFill.setB(b); }},
+    //        {"C", [&toFill](bool c){ toFill.setC(c); } }
+    //    },
+    //    {} //empty child tag list
+    //};
     return 0;
 }
 
